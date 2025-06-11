87f99c50d833af6686434c283b8d039c4e203a81
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-06-11 20:22:54 +0300

    add search conversations

--- frontend/src/components/sidebar/SearchInput.tsx
+++ frontend/src/components/sidebar/SearchInput.tsx
@@ -1,17 +1,48 @@
 import { Search } from "lucide-react";
+import { useState, type FormEvent } from "react";
+import useGetConversations from "../../hooks/useGetConversations";
+import useConversation from "../../store/useConversation";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
-	return (
-		<form className='flex items-center gap-2'>
-			<input
-				type='text'
-				placeholder='Search…'
-				className='input-sm md:input input-bordered rounded-full sm:rounded-full w-full'
-			/>
-			<button type='submit' className='btn md:btn-md btn-sm btn-circle bg-sky-500 text-white  '>
-				<Search className='w-4 h-4 md:w-6 md:h-6 outline-none' />
-			</button>
-		</form>
-	);
+  const [search, setSearch] = useState("");
+  const { conversations } = useGetConversations();
+  const { setSelectedConversation } = useConversation();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!search.trim()) return;
+
+    if (search.length < 3) {
+      return toast.error("Search term must be at least 3 characters long");
+    }
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No such user found!");
+  };
+
+  return (
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search…"
+        className="input-sm md:input input-bordered rounded-full sm:rounded-full w-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="btn md:btn-md btn-sm btn-circle bg-sky-500 text-white  "
+      >
+        <Search className="w-4 h-4 md:w-6 md:h-6 outline-none" />
+      </button>
+    </form>
+  );
 };
 export default SearchInput;
