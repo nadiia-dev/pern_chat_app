5d8132d8622bbd8e19adc149a6e69de7407aa822
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-06-12 09:59:38 +0300

    add send realtime messages

--- backend/src/controllers/messages.controller.ts
+++ backend/src/controllers/messages.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../db/prisma";
+import { getReceiverSocketId, io } from "../socket/socket";
 
 export const sendMessage = async (
   req: Request,
@@ -47,6 +48,12 @@ export const sendMessage = async (
           },
         },
       });
+    }
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
     }
 
     res.status(201).json(newMessage);
